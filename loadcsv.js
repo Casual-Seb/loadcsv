e052d9938bd741ddaa9e4d31b12c86f34791f6b1
Sebastian <sebastian@nomme.com>
2020-04-06 02:12:56 +0200

    Added Seed and test split options

--- loadcsv.js
+++ loadcsv.js
@@ -1,8 +1,19 @@
 const fs = require("fs");
 const { Transform } = require("stream");
 const _ = require("lodash");
+const shuffleSeed = require("shuffle-seed");
 
-function readStream(filename, { converters = {} }) {
+function readStream(
+  filename,
+  {
+    converters = {},
+    dataColumns = [],
+    labelColumns = [],
+    shuffle = true,
+    seed = "standard",
+    splitTest = false,
+  }
+) {
   return new Promise((resolve, reject) => {
     let data = [];
     stream = fs
@@ -11,7 +22,37 @@ function readStream(filename, { converters = {} }) {
     stream.on("data", (chunk) => {
       data.push(chunk);
     });
-    stream.on("end", () => resolve(data.flat(1)));
+
+    const fix = () => {
+      data = data.flat(1);
+      let labels = extractColumns(data, labelColumns);
+      data = extractColumns(data, dataColumns);
+
+      data.shift();
+      labels.shift();
+
+      if (shuffle) {
+        data = shuffleSeed.shuffle(data, seed);
+        labels = shuffleSeed.shuffle(labels, seed);
+      }
+
+      if (splitTest) {
+        const trainSize = _.isNumber(splitTest)
+          ? splitTest
+          : Math.floor(data.length / 2);
+
+        return {
+          features: data.slice(0, trainSize),
+          labels: labels.slice(0, trainSize),
+          testFeatures: data.slice(trainSize),
+          testLabels: labels.slice(trainSize),
+        };
+      } else {
+        return { features: data, labels };
+      }
+    };
+
+    stream.on("end", () => resolve(fix()));
     stream.on("error", (error) => reject(error));
   });
 }
@@ -46,13 +87,32 @@ function clean(converters) {
   });
 }
 
+function extractColumns(data, columnNames) {
+  const headers = _.first(data);
+
+  const indexes = _.map(columnNames, (column) => headers.indexOf(column));
+  const extracted = _.map(data, (row) => _.pullAt(row, indexes));
+
+  return extracted;
+}
+
 async function loadCSV(filename, options) {
   console.log("calling");
   const result = await readStream(filename, options);
-  console.log(result);
+  const { features, labels, testFeatures, testLabels } = result;
+
+  console.log("Features:", features);
+  console.log("Labels:", labels);
+  console.log("TestFeatures:", testFeatures);
+  console.log("TestLabels:", testLabels);
 }
 
 loadCSV("data.csv", {
+  dataColumns: ["height", "value"],
+  labelColumns: ["passed"],
+  seed: "sdfsdf",
+  shuffle: true,
+  splitTest: 2,
   converters: {
     passed: (val) => (val === "TRUE" ? 1 : 0),
   },
